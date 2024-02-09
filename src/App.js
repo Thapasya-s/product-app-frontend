c40944d02ec7ca78f74319708dcfe6d9a6966a6f
Thapasya-s <thapasyasajee@gmail.com>
2024-02-09 09:24:44 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewProduct from './components.jsx/ViewProduct';
+import AddPro from './components.jsx/AddPro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddPro/>}/>
+        
+        <Route path='/view' element={<ViewProduct/>}/>
+        </Routes></BrowserRouter>
     </div>
   );
 }
